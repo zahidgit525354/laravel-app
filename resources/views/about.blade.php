e05a627ccb7d9ef340250d6a312ca904b0d954b7
Zahid <zahid@Zahids-MacBook-Pro.local>
2025-05-20 19:54:20 -0700

    Add about.php and route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/about.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Page</title>
+</head>
+<body>
+  <h1>Welcome to the About Page</h1>
+  <p>This is a simple static page in Laravel.</p>
+</body>
+</html>
